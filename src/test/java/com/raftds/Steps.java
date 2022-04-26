bd9a8a46eb10904d20664fc10c4d49ae57554b1a
brodu <brodu0110@gmail.com>
2022-04-26 17:39:14 +0300

    added WebDriverSettings.java class, moved hooks here

--- src/test/java/com/raftds/Steps.java
+++ src/test/java/com/raftds/Steps.java
@@ -1,24 +1,10 @@
 package com.raftds;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
+import static com.raftds.WebDriverSettings.chromeDriver;
 
 public class Steps {
-    public ChromeDriver chromeDriver;
-
-    @Before
-    public void beforeHook() {
-        System.setProperty("webdriver.chrome.driver", "/Users/brodu/Webdrivers/chromedriver");
-        chromeDriver = new ChromeDriver();
-    }
-
-    @After
-    public void afterHook() {
-        chromeDriver.quit();
-    }
 
     @Test
     public void test() {
